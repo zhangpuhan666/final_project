97889563caf778444b151fb30f17419f06f787c6
Puhan Zhang <pz2271@columbia.edu>
2021-04-11 07:32:23 +0000

    add into the admin and change admin.py`

--- squirrel/models.py
+++ squirrel/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext as _
 
-class Sightings(models.Model):
+class Sighting(models.Model):
     Latitude = models.FloatField(
         help_text = _('Latitude'),)
 
